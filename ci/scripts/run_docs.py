acdb2741f366f12d75c94ce424215f9708c29bc2
william.li <william.li@ll.mit.edu>
2023-12-14 09:08:19 -0500

    Updating run_docs.py [x] Added code for generating HTML
    Former-commit-id: fc15cf905d528ce17e64562e88e43bf1255d0a7e

--- ci/scripts/run_docs.py
+++ ci/scripts/run_docs.py
@@ -1,3 +1,36 @@
 import os
+import webbrowser
 
-os.system("pdoc3 --html src")
+
+def generate_html():
+    """Generate the HTML files that are necessary to display the information."""
+
+    os.system("mkdir -p docs/html/src/")
+    os.system("cp -rf docs/pics docs/html/src/")
+
+    # Modern way to open files. The closing in handled cleanly
+    with open("README.md", mode="r") as in_file, open("src/__init__.py", mode="w") as out_file:
+        # Header
+        out_file.write('"""')
+
+        # Write
+        for in_line in in_file:
+            mod_line = in_line.replace("docs/pics/", "pics/")
+            out_file.write(mod_line)
+
+        # Footer
+        out_file.write('"""')
+
+    os.system("pdoc3 --html src --template-dir ci/templates/ -o ./docs/html --force")
+
+
+def show_docs():
+    """Generate the HTML webpage and open a browser to display it."""
+
+    generate_html()
+    cwd = os.getcwd()
+    webbrowser.get("chrome").open(f"file://{cwd}/docs/html/src/index.html")
+
+
+if __name__ == "__main__":
+    show_docs()
