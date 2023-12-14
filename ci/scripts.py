1db179c1a876cc074b22818b3f5d31c525d0c0ce
william.li <william.li@ll.mit.edu>
2023-12-14 17:13:30 -0500

    Updating CI tests [x] Updated nox tests for all CI common tests [x] Removed old scripts [x] Consolidated old scripts into a single file scripts.py which is located in ci/ [x] Added a basic_test.py for running pytests
    Former-commit-id: 2c981c0cfc1e919dbb312966c1d035c736057270

---
+++ ci/scripts.py
@@ -0,0 +1,76 @@
+"""
+Common functions that are executed when running Continuous Integration (CI) include:
+
+* pytests: tests to verify code
+* coverage: coverage of code given pytests
+* profiling: profiling of certain functions
+* autodocs: automatic documentation generation
+
+"""
+import subprocess
+import os
+
+
+def pytest():
+    """Run pytests."""
+    subprocess.run(["pytest"])
+
+
+def coverage():
+    """Run coverage of code based on pytests."""
+    # Run coverage using pytest, then record results to docs.
+    subprocess.run(["coverage", "run", "-m", "pytest"])
+    subprocess.run(["coverage", "html", "-d", "save/coverage"])
+    subprocess.run(["coverage", "report", "-m"])
+
+
+def profile():
+    """Profiles your selected code using scalene."""
+
+    cwd = os.getcwd()
+    subprocess.run(["mkdir", "-p", "save/profile"])
+
+    # Profile the code.
+    subprocess.run(
+        [
+            "scalene",
+            "--outfile",
+            f"{cwd}/save/profile/profile.html",
+            "--html",
+            "-m",
+            "pytest",
+        ],
+        shell=True,
+        env={
+            "LINES": "25",
+            "COLUMNS": "200",
+        },
+    )
+
+
+def autodoc():
+    """Generate automatic documentation."""
+
+    subprocess.run(["mkdir", "-p", "html/docs"])
+    subprocess.run(["cp", "-rf", "docs/pics", "html/docs/"])
+    subprocess.run(
+        [
+            "pdoc",
+            "--logo",
+            "https://github.com/destin-v/vs_codex/blob/main/docs/pics/program_logo.png?raw=true",
+            "--logo-link",
+            "https://github.com/destin-v/vs_codex",
+            "--footer-text",
+            "Author: W. Li",
+            "--output-directory",
+            "html",
+            "src",
+        ]
+    )
+
+
+if __name__ == "__main__":
+    # pytest()
+    # coverage()
+    profile()
+    # autodoc()
--- noxfile.py
+++ noxfile.py
@@ -4,15 +4,22 @@ Nox creates a new virtual environment for each individual test.  Thus, it is imp
 Useful commands:
 
 ```console
-nox --list          # Lists out all the available sessions
-nox -s pytest       # Runs the session for pytest
+nox --list              # Lists out all the available sessions
+nox -s pytest           # Run pytests
+nox -s coverage         # Run coverage
+nox -s profile          # Profile the code
+nox -s documentation    # Generate documentation
+
+nox                     # Run all sessions
 ```
 
 """
+import subprocess
+
 import nox
 
 
-@nox.session(python=["3.8.0", "3.9.0"])
+@nox.session(python=["3.8", "3.9", "3.10"])
 def pytest(session):
     """Run PyTests."""
 
@@ -36,23 +43,37 @@ def profile(session):
     """Profiles your selected code using scalene."""
 
     session.run("poetry", "install")
-
+    session.run("mkdir", "-p", "save/profile")
     session.run(
         "scalene",
         "--outfile",
-        "save/profile.html",
+        "save/profile/profile.html",
         "--html",
-        "main.py",
-        env={
-            "LINES": "25",
-            "COLUMNS": "200",
-        },
+        "-m",
+        "pytest",
+        env={"LINES": "25", "COLUMNS": "200"},
     )
 
 
 @nox.session
-def documentation(session):
+def autodoc(session):
     """Generate automatic documentation."""
 
-    session.run("poetry", "install")
-    session.run("pdoc3", "--html", "src")
+    session.run("pip", "install", "pdoc")
+    subprocess.run(["mkdir", "-p", "html/docs"])
+    subprocess.run(["cp", "-rf", "docs/pics", "html/docs/"])
+
+    subprocess.run(
+        [
+            "pdoc",
+            "--logo",
+            "https://github.com/destin-v/vs_codex/blob/main/docs/pics/program_logo.png?raw=true",
+            "--logo-link",
+            "https://github.com/destin-v/vs_codex",
+            "--footer-text",
+            "Author: W. Li",
+            "--output-directory",
+            "html",
+            "src",
+        ]
+    )
